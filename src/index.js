76a776381baf3a37620766eca1f6a08a0943f035
pedrotfs <pedrotfs@gmail.com>
2020-04-23 13:24:54 -0300

    task-02 endpoints

--- src/index.js
+++ src/index.js
@@ -19,12 +19,50 @@ app.post("/users", (req, res) => {
     })
 })
 
+app.get("/users", (req, res) => {
+    User.find({}).then((result) => {
+        res.send(result)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get("/users/:id", (req, res) => {
+    User.findById(req.params.id).then((result) => {
+        if(!result) {
+            return res.status(404).send()
+        }
+        res.send(result)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
 app.post("/tasks", (req, res) => {
     const task = new Task(req.body)
     task.save().then(() => {
         res.status(201).send(task)
     }).catch((e) => {
         res.status(400).send(e)
+    })
+})
+
+app.get("/tasks", (req, res) => {
+    Task.find({}).then((result) => {
+        res.send(result)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get("/tasks/:id", (req, res) => {
+    Task.findById(req.params.id).then((result) => {
+        if(!result) {
+            return res.status(404).send()
+        }
+        res.send(result)
+    }).catch((e) => {
+        res.status(500).send()
     })
 })
 
